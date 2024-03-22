fdaf4cc93d907f082dc0d747ecfebff5244c90fc
starcat37 <starcat37@korea.ac.kr>
2024-03-22 23:48:47 +0900

    build: config, typeorm 설치 및 mysql 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      port: 3306,
+      host: process.env.MYSQL_HOST,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      multipleStatements: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
