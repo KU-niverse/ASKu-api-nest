1040e0b9cecd704c5ec996abc1f5c8436b66272b
youngsupchoi <sup1214@naver.com>
2024-04-03 07:11:05 +0900

    Feat: wiki_doc entity

--- src/wiki/wiki.module.ts
+++ src/wiki/wiki.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WikiDoc } from 'src/wiki/entities/wikiDoc.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([WikiDoc])],
+})
 export class WikiModule {}
