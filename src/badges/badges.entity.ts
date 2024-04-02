076fb4020c1537a8dcd2bdabb77efb1aa0a21861
youngsupchoi <sup1214@naver.com>
2024-04-03 07:02:24 +0900

    Fix: badge entity BaseEntity 및 nullable옵션 추가

--- src/badges/badges.entity.ts
+++ src/badges/badges.entity.ts
@@ -1,8 +1,8 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('badges')
-export class Badge {
+export class Badge extends BaseEntity {
   @PrimaryGeneratedColumn()
   @ApiProperty({ description: '배지의 고유 식별자' })
   id: number;
@@ -10,20 +10,22 @@ export class Badge {
   @Column({
     type: 'varchar',
     length: 20,
+    nullable: false,
   })
   @ApiProperty({ description: '배지의 이름', maxLength: 20 })
   name: string;
 
-  @Column('text')
+  @Column({ type: 'text', nullable: false })
   @ApiProperty({ description: '로컬에 저장된 배지 이미지의 링크' })
   image: string;
 
-  @Column('text')
+  @Column({ type: 'text', nullable: false })
   @ApiProperty({ description: '배지에 대한 설명' })
   description: string;
 
   @Column({
     type: 'bool',
+    nullable: false,
     default: 0,
   })
   @ApiProperty({ description: '[배지의 특성] 0: 일반, 1: 이벤트', default: 0 })
@@ -31,6 +33,7 @@ export class Badge {
 
   @Column({
     type: 'bool',
+    nullable: false,
     default: 0,
   })
   @ApiProperty({ description: '[배지의 특성] 0: 단일, 1: 연속', default: 0 })
