eead3e51d3217fef269c1bdeb98be7b406bc319c
youngsupchoi <sup1214@naver.com>
2024-04-03 07:40:54 +0900

    Feat: answer entity

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Question } from 'src/question/question.entity';
+import { Answer } from 'src/question/entities/answer.entity';
+import { Question } from 'src/question/entities/question.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Question])],
+  imports: [TypeOrmModule.forFeature([Question, Answer])],
 })
 export class QuestionModule {}
