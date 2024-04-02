a6a24986f1a4a32ed0f93242340f6647d397368d
youngsupchoi <sup1214@naver.com>
2024-04-03 07:30:14 +0900

    Feat: wikiHistory entity

--- src/wiki/wiki.module.ts
+++ src/wiki/wiki.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WikiDoc } from 'src/wiki/entities/wikiDoc.entity';
 import { WikiFavorites } from 'src/wiki/entities/wikiFavorites';
+import { WikiHistory } from 'src/wiki/entities/wikiHistory.entity';
 import { WikiDocsView } from 'src/wiki/entities/wikiView.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WikiDoc, WikiDocsView, WikiFavorites])],
+  imports: [
+    TypeOrmModule.forFeature([
+      WikiDoc,
+      WikiDocsView,
+      WikiFavorites,
+      WikiHistory,
+    ]),
+  ],
 })
 export class WikiModule {}
