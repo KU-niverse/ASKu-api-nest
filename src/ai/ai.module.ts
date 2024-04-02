c1213b5acf62229daed73de89e4f1a473533ad6d
youngsupchoi <sup1214@naver.com>
2024-04-03 08:17:55 +0900

    Feat: feedbackContent entity

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -3,8 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AiHistory } from 'src/ai/entities/aiHistory.entity';
 import { AiSession } from 'src/ai/entities/aiSession.entity';
 import { Feedback } from 'src/ai/entities/feedback.entity';
+import { FeedbackContent } from 'src/ai/entities/feedbackContent.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AiSession, AiHistory, Feedback])],
+  imports: [
+    TypeOrmModule.forFeature([AiSession, AiHistory, Feedback, FeedbackContent]),
+  ],
 })
 export class AiModule {}
