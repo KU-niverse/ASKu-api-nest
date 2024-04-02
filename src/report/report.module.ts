a6bf97f39f9d5c462431f9274925979e65bc7ca2
youngsupchoi <sup1214@naver.com>
2024-04-03 08:15:26 +0900

    Feat: reportType entity

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportType } from 'src/report/entities/reportType.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ReportType])],
+})
 export class ReportModule {}
