44bdd72c0119091f01df32531922a5fb3027fcb4
youngsupchoi <sup1214@naver.com>
2024-04-03 08:19:27 +0900

    Feat: reportReason entity

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportReason } from 'src/report/entities/reportReason.entity';
 import { ReportType } from 'src/report/entities/reportType.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReportType])],
+  imports: [TypeOrmModule.forFeature([ReportType, ReportReason])],
 })
 export class ReportModule {}
