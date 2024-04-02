245db7b1a2d7be426e8ca44b5ce8a47a4f3c2ce7
youngsupchoi <sup1214@naver.com>
2024-04-03 07:32:06 +0900

    Feat: debate entity

--- src/debate/debate.module.ts
+++ src/debate/debate.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Debate } from 'src/debate/debate.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Debate])],
+})
 export class DebateModule {}
