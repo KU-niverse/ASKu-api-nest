a283f6d5542789506c3e8b972ba4cba2f8062113
choi <cws@naver.com>
2024-04-07 03:22:09 +0900

    get user/me/info api 추가

---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async getMyInfo(id: number): Promise<User> {
+    const result = await this.userRepository.findOne({ where: { id } });
+    if (!result) {
+      throw new NotFoundException('해당 ID를 가진 유저가 존재하지 않습니다.');
+    }
+    return result;
+  }
+}
