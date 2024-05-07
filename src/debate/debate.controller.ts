e4760197711abd13fb404c76d690c990fff6aa6d
이나라 <eddie1996@naver.com>
2024-05-07 19:22:50 +0900

    debate_all_recent api 구현

--- src/debate/debate.controller.ts
+++ src/debate/debate.controller.ts
@@ -1,42 +1,29 @@
-import { Controller, Get, HttpCode, Param } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { DebateService } from './debate.service';
-import { DebateHistory } from './entities/debateHistory.entity';
 import { Debate } from './entities/debate.entity';
 
 @Controller('debate')
 export class DebateController {
   constructor(private readonly debateService: DebateService) {}
-  @Get('me/debatehistory/:userId')
-  @HttpCode(201)
+  @Get('all/recent')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({
-    summary: '유저 토론 히스토리',
-    description: '유저의 토론 히스토리를 조회합니다.',
+    summary: '전체 토론방 목록 조회',
+    description: '전체 토론방 목록 조회 성공',
   })
   @ApiResponse({
-    status: 201,
-    description: '위키 히스토리 불러오기 성공',
+    status: 200,
+    description: '전체 토론방 목록 조회 성공',
     type: Debate,
-  })
-  @ApiResponse({
-    status: 400,
-    description: '잘못된 요청입니다.',
-  })
-  @ApiResponse({
-    status: 401,
-    description: '유저 로그인 되어있지 않은 상태',
-  })
-  @ApiResponse({
-    status: 404,
-    description: '해당 ID를 가진 유저가 존재하지 않습니다.',
+    isArray: true,
   })
   @ApiResponse({
     status: 500,
-    description: '서버 에러',
+    description: '오류가 발생했습니다.',
   })
-  getMyDebateHistory(
-    @Param('userId') userId: number,
-  ): Promise<DebateHistory[]> {
-    return this.debateService.getMyDebateHistory(userId);
+  getAllDebateByEdit(): Promise<Debate[]> {
+    return this.debateService.getAllDebateByEdit();
   }
+
 }
--- src/debate/debate.service.ts
+++ src/debate/debate.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { DebateHistory } from './entities/debateHistory.entity';
-
+import { Debate } from './entities/debate.entity';
 @Injectable()
 export class DebateService {
   constructor(
-    @InjectRepository(DebateHistory)
-    private debateRepository: Repository<DebateHistory>,
+    @InjectRepository(Debate)
+    private debateRepository: Repository<Debate>,
   ) {}
 
-  async getMyDebateHistory(userId: number): Promise<DebateHistory[]> {
-    const result: DebateHistory[] = await this.debateRepository.find({
-      where: { userId },
-      relations: ['debate'],
-    });
-    return result;
+  async getAllDebateByEdit(): Promise<Debate[]> {
+    const debate: Debate[] = await this.debateRepository
+      .createQueryBuilder('debate')
+      .innerJoinAndSelect('debate.wikiDoc', 'wikiDoc')
+      .select([
+        'debate',
+        'wikiDoc.title',
+      ])
+      .orderBy('debate.recentEditedAt', 'DESC')
+      .getMany();
+  
+    return debate;
   }
 }
