4e8cd07c68b70118f429b612d73c8e8547d72313
youngsupchoi <sup1214@naver.com>
2024-05-09 00:46:14 +0900

    Fix: 충돌 수정

--- src/badge/badge.module.ts
+++ src/badge/badge.module.ts
@@ -4,9 +4,10 @@ import { Badge } from 'src/badge/entities/badge.entity';
 
 import { BadgeController } from './badge.controller';
 import { BadgeService } from './badge.service';
+import { BadgeHistory } from 'src/badge/entities/badgeHistory.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Badge])],
+  imports: [TypeOrmModule.forFeature([Badge, BadgeHistory])],
   controllers: [BadgeController],
   providers: [BadgeService],
   exports: [TypeOrmModule, BadgeService],
