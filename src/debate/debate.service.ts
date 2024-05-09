9e3891d0b68de6c71f9dac68d4540b940d547c15
이나라 <eddie1996@naver.com>
2024-05-09 21:08:20 +0900

    feat: all_recent 기존 api 복구

--- src/debate/debate.service.ts
+++ src/debate/debate.service.ts
@@ -1,17 +1,29 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Debate } from './entities/debate.entity';
 import { DebateHistory } from './entities/debateHistory.entity';
+import { Debate } from './entities/debate.entity';
+
 @Injectable()
 export class DebateService {
   constructor(
     @InjectRepository(DebateHistory)
     private debateRepository: Repository<DebateHistory>,
+    @InjectRepository(Debate)
+    private debate: Repository<Debate>,
   ) {}
 
+  async getMyDebateHistory(userId: number): Promise<DebateHistory[]> {
+    const result: DebateHistory[] = await this.debateRepository.find({
+      where: { userId },
+      relations: ['debate'],
+    });
+    return result;
+  }
+
+
   async getAllDebateByEdit(): Promise<Debate[]> {
-    const debate: Debate[] = await this.debateRepository
+    const debate: Debate[] = await this.debate
       .createQueryBuilder('debate')
       .innerJoinAndSelect('debate.wikiDoc', 'wikiDoc')
       .select([
@@ -23,7 +35,6 @@ export class DebateService {
   
     return debate;
   }
-
 
 }
 
