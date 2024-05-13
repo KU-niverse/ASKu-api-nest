e36023aca59e40ccdc58c94d70e205d38e24e44a
choi <cws@naver.com>
2024-05-13 12:43:02 +0900

    Fix: description 수정

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -83,32 +83,18 @@ export class QuestionController {
   @Get('veiw/flag/:title')
   @HttpCode(HttpStatus.CREATED)
   @ApiOperation({
-    summary: '유저 질문 히스토리',
-    description: '유저 질문 히스토리를 조회합니다.',
+    summary: '질문 목록 조회',
+    description: '질문 목록을 조회하였습니다.',
   })
   @ApiResponse({
     status: 200,
-    description: '유저 질문 히스토리 조회를 성공했습니다.',
+    description: '질문 목록을 조회하였습니다.',
     type: Question,
     isArray: true,
   })
   @ApiResponse({
-    status: 404,
-    description:
-      '해당 ID를 가진 유저가 존재하지 않습니다. 유효한 유저 ID를 입력해주세요.',
-  })
-  @ApiResponse({
     status: 400,
-    description: '잘못된 요청입니다. 질문 히스토리 불러오기에 실패하였습니다.',
-  })
-  @ApiResponse({
-    status: 401,
-    description: '인증되지 않은 사용자입니다. 로그인이 필요합니다.',
-  })
-  @ApiResponse({
-    status: 403,
-    description:
-      '권한이 없습니다. 해당 유저의 질문 히스토리를 조회할 수 있는 권한이 없습니다.',
+    description: '잘못된 flag 값입니다.',
   })
   @ApiResponse({
     status: 500,
