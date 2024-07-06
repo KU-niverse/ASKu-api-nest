6607599ac0e839eadd2926960686a96d798b0dee
CHOI <cws98tiger@naver.com>
2024-07-07 04:34:57 +0900

    fix: API QA

--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -248,7 +248,6 @@ export class QuestionService {
     if (!rows.length) {
       throw new NotFoundException('No popular questions found');
     } else {
-      console.log('3');
       return rows;
     }
     return rows;
