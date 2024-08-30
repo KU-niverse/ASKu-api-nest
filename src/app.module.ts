ee2b7a43649258ce789587f32208224613273aa9
Junhyun An <68099714+DDididingding@users.noreply.github.com>
2024-08-30 20:06:04 +0900

    modify : unused import 주석처리
    app.module.ts
    line 20 : QuestionService import

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AuthModule } from './auth/auth.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
-import { QuestionService } from './question/question.service';
+// import { QuestionService } from './question/question.service';
 
 @Module({
   imports: [
