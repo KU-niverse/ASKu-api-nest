27ce0a11bd46560cf0806397b6c0002a07a82eb8
Junhyun An <68099714+DDididingding@users.noreply.github.com>
2024-08-30 20:11:15 +0900

    modify : Service레이어에 getUserConributions 추가
    + repository 로직 분리하여 코드 간결화

--- src/wiki/wiki.service.ts
+++ src/wiki/wiki.service.ts
@@ -1,21 +1,54 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { WikiRepository } from './wiki.repository';
+import { ContributionsResponseDto } from './dto/contributions-response.dto';
 import { WikiHistory } from './entities/wikiHistory.entity';
-import { Repository } from 'typeorm';
+import { UserRepository } from '../user/user.repository';
 
 @Injectable()
 export class WikiService {
   constructor(
-    @InjectRepository(WikiHistory)
-    private wikiHistoryRepository: Repository<WikiHistory>,
+    private wikiRepository: WikiRepository,
+    private userRepository: UserRepository,
   ) {}
 
   async getWikiHistoryByUserId(userId: number): Promise<WikiHistory[]> {
-    const wikiHistory: WikiHistory[] = await this.wikiHistoryRepository.find({
-      where: { userId: userId },
-      relations: ['wikiDoc'],
-    });
-    return wikiHistory;
+    return this.wikiRepository.getWikiHistoryByUserId(userId);
   }
 
+  async getUserContributions(
+    userId: number,
+  ): Promise<ContributionsResponseDto> {
+    // 전체 유저 수 조회
+    const totalUsers = await this.userRepository.getTotalUsers();
+
+    // 유저 랭킹 및 포인트 조회
+    const userRankingAndPoint =
+      await this.userRepository.getUserRankingAndPoint(userId);
+
+    const { ranking, user_point } = userRankingAndPoint;
+
+    // 랭킹 퍼센티지 계산
+    let ranking_percentage = (ranking / totalUsers) * 100;
+    if (user_point === 0) {
+      ranking_percentage = 100;
+    }
+
+    // 문서별 기여도 조회
+    const docsContributions = await this.wikiRepository.getDocsContributions(
+      userId,
+      user_point,
+    );
+
+    return {
+      count: totalUsers,
+      ranking,
+      point: user_point,
+      ranking_percentage: Number(ranking_percentage.toFixed(4)),
+      docs: docsContributions.map((doc) => ({
+        ...doc,
+        doc_point: doc.doc_point.toString(),
+        percentage: doc.percentage.toString(),
+      })),
+    };
+  }
 }
