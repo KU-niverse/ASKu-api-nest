049d250061c185e15ecaf09484f6519528106096
Junhyun An <68099714+DDididingding@users.noreply.github.com>
2024-08-30 20:11:57 +0900

    feature : wiki/contribution api 작성.
    + swagger 태그 추가

--- src/wiki/wiki.controller.ts
+++ src/wiki/wiki.controller.ts
@@ -7,11 +7,13 @@ import {
 } from '@nestjs/common';
 import { WikiService } from './wiki.service';
 import { WikiHistory } from './entities/wikiHistory.entity';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ContributionsResponseDto } from './dto/contributions-response.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/user/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 
+@ApiTags('wiki')
 @Controller('wiki')
 export class WikiController {
   constructor(private readonly wikiService: WikiService) {}
@@ -39,5 +41,30 @@ export class WikiController {
   })
   getWikiHistoryByUserId(@GetUser() user: User): Promise<WikiHistory[]> {
     return this.wikiService.getWikiHistoryByUserId(user.id);
+  }
+
+  @Get('contributions')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard())
+  @ApiOperation({
+    summary: '유저의 문서별 기여도',
+    description: '로그인한 유저의 문서별 기여도를 조회합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '기여도 조회 성공',
+  })
+  @ApiResponse({
+    status: 401,
+    description: '유저 로그인 되어있지 않은 상태',
+  })
+  @ApiResponse({
+    status: 500,
+    description: '서버 에러',
+  })
+  getUserContributions(
+    @GetUser() user: User,
+  ): Promise<ContributionsResponseDto> {
+    return this.wikiService.getUserContributions(user.id);
   }
 }
