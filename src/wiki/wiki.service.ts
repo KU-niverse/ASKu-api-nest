11430b4a5dd13ba2161afbcfdff09c8bb416875d
Junhyun An <68099714+DDididingding@users.noreply.github.com>
2024-08-30 20:11:15 +0900

    Merge : S3를 활용한 코드와 병합

--- src/wiki/wiki.service.ts
+++ src/wiki/wiki.service.ts
@@ -1,3 +1,6 @@
+import { WikiRepository } from './wiki.repository';
+import { UserRepository } from '../user/user.repository';
+import { ContributionsResponseDto } from './dto/contributions-response.dto';
 import {
   Injectable,
   InternalServerErrorException,
@@ -20,6 +23,7 @@ import { Readable } from 'stream';
 
 const edp = 'https://kr.object.ncloudstorage.com/';
 const region = 'kr-standard';
+
 
 @Injectable()
 export class WikiService {
@@ -44,13 +48,12 @@ export class WikiService {
       },
     });
   }
+    private wikiRepository: WikiRepository,
+    private userRepository: UserRepository,
+  ) {}
 
   async getWikiHistoryByUserId(userId: number): Promise<WikiHistory[]> {
-    const wikiHistory: WikiHistory[] = await this.wikiHistoryRepository.find({
-      where: { userId: userId },
-      relations: ['wikiDoc'],
-    });
-    return wikiHistory;
+    return this.wikiRepository.getWikiHistoryByUserId(userId);
   }
 
   async getAllWikiDocs(): Promise<string[]> {
@@ -500,5 +503,41 @@ export class WikiService {
         status: 500,
       };
     }
+  }
+  async getUserContributions(
+    userId: number,
+  ): Promise<ContributionsResponseDto> {
+    // 전체 유저 수 조회
+    const totalUsers = await this.userRepository.getTotalUsers();
+
+    // 유저 랭킹 및 포인트 조회
+    const userRankingAndPoint =
+      await this.userRepository.getUserRankingAndPoint(userId);
+
+    const { ranking, user_point } = userRankingAndPoint;
+
+    // 랭킹 퍼센티지 계산
+    let ranking_percentage = (ranking / totalUsers) * 100;
+    if (user_point === 0) {
+      ranking_percentage = 100;
+    }
+
+    // 문서별 기여도 조회
+    const docsContributions = await this.wikiRepository.getDocsContributions(
+      userId,
+      user_point,
+    );
+
+    return {
+      count: totalUsers,
+      ranking,
+      point: user_point,
+      ranking_percentage: Number(ranking_percentage.toFixed(4)),
+      docs: docsContributions.map((doc) => ({
+        ...doc,
+        doc_point: doc.doc_point.toString(),
+        percentage: doc.percentage.toString(),
+      })),
+    };
   }
 }
