e16d678f0ee11f368e1216a60cb3464e3e7f8345
youngsupchoi <sup1214@naver.com>
2024-09-08 15:41:58 +0900

    Fix: 로그인 api 문서화 및 출력 형식 통일

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
@@ -8,19 +9,21 @@ import {
 import { Not } from 'typeorm';
 
 export class AuthCredentialsDto {
-  // @IsString()
   // @MinLength(4)
   // @MaxLength(20)
+  @ApiProperty({ description: '로그인 아이디', default: 'qwer1234' })
   @IsNotEmpty()
+  @IsString()
   login_id: string;
-  // @IsString()
+
   // @MinLength(4)
   // @MaxLength(20)
   //영어랑 숫자만 가능한 유효성 체크
   // @Matches(/^[a-zA-Z0-9]*$/, {
   //   message: 'password only accepts english and numbers',
   // })
-
+  @ApiProperty({ description: '비밀번호', default: 'qwer1234@' })
   @IsNotEmpty()
+  @IsString()
   password: string;
 }
