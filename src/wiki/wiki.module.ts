116a4f363d02b5a7aed3e0ab196d46df32ab3277
Junhyun An <alloy0301@gmail.com>
2024-10-28 19:13:41 +0900

    mod: delete useless import

--- src/wiki/wiki.module.ts
+++ src/wiki/wiki.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WikiDoc } from 'src/wiki/entities/wikiDoc.entity';
 import { WikiFavorites } from 'src/wiki/entities/wikiFavorites';
