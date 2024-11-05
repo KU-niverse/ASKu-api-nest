b4405b6eceedbafd3c3773fdc818e95a94fb670f
Supernova0417 <wlsrnjs7013@korea.ac.kr>
2024-11-05 21:40:00 +0900

    fix: 누락된 search/popular API 생성

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SearchController } from 'src/search/search.controller';
+import { SearchService } from 'src/search/search.service';
 import { SearchHistory } from 'src/search/searchHistory.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SearchHistory])],
+  controllers: [SearchController],
+  providers: [SearchService],
 })
 export class SearchModule {}
