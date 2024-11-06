9f76a89e68569b87a64ec84138f837bb75e784e0
youngsupchoi <sup1214@naver.com>
2024-11-06 21:38:21 +0900

    Fix: cors설정 수정

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:3000', 'http://61.109.238.126:8080'],
     credentials: true,
     exposedHeaders: ['Authorization'],
   });
