ec975e57c200c240bba3ecb8129ffa4ace4049e9
Supernova0417 <wlsrnjs7013@korea.ac.kr>
2024-11-11 04:30:36 +0900

    fix: fix minor typos

--- src/badge/badge.controller.ts
+++ src/badge/badge.controller.ts
@@ -22,7 +22,7 @@ export class BadgeController {
   @UseGuards(AuthGuard())
   @HttpCode(HttpStatus.CREATED)
   @ApiOperation({
-    summary: '모든 배지',
+    summary: '모든 배지 정보',
     description: '존재하는 모든 배지를 조회합니다.',
   })
   @ApiResponse({
