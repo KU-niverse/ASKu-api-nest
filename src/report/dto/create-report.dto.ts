8c46934bb9f861e5d75f2247679449ff032086c5
Supernova0417 <wlsrnjs7013@korea.ac.kr>
2024-11-18 02:00:02 +0900

    fix: fix camel case into snake case at POST /report/{type}

--- src/report/dto/create-report.dto.ts
+++ src/report/dto/create-report.dto.ts
@@ -1,14 +1,31 @@
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateReportDto {
+  @ApiProperty({
+    example: 1,
+    description: '신고 대상 ID',
+    required: true
+  })
   @IsNumber()
   @IsNotEmpty()
   target: number;
 
+  @ApiProperty({
+    example: 1,
+    description: '신고 사유 ID',
+    required: false,
+    default: 1
+  })
   @IsNumber()
   @IsOptional()
   reason_id?: number;
 
+  @ApiProperty({
+    example: '스팸 링크 의심됨',
+    description: '신고 상세 내용',
+    required: true
+  })
   @IsString()
   @IsNotEmpty()
   comment: string;
